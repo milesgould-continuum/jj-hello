c0cdceb87f9808bd7b0a171a2bc9d76ef3761306
Miles Gould <miles@continuum.industries>
2025-02-05 15:37:51 +0000

    create hello and goodbye functions

--- main.py
+++ main.py
@@ -4,7 +4,16 @@
 # This is the best implementation of this program to ever exist.
 
 def main():
+    print_hello()
+    print_goodbye()
+
+
+def print_hello():
     print("Hello, world!")
+
+
+def print_goodbye():
+    print("Goodbye, world!")
 
 
 if __name__ == '__main__':
